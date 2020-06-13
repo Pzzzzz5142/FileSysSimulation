bb2ac01814894156f0afcb6b8175393551b34a44
Pzzzzz <hello-cd.plus@hotmail.com>
2020-06-13 19:13:28 +0800

    iget完善
    Signed-off-by: Pzzzzz <“hello-cd.plus@hotmail.com”>

--- igetput.cpp
+++ igetput.cpp
@@ -4,6 +4,19 @@
 
 #include "filesys.h"
 
+void undirect(int loc, int lv) {
+    int buff[BLOCKSIZ / sizeof(unsigned int)];
+    fs.seekp(BLOCKSIZ * loc + DATASTART, ios::beg);
+    fs.read((char *) buff, sizeof(BlockCharge));
+    int len = sizeof(buff) / sizeof(int);
+    for (int i = 0; i < len && buff[i] != -1; i++) {
+        if (lv != 0)
+            undirect(buff[i], lv - 1);
+        else
+            bfree(buff[i]);
+    }
+}
+
 inode *iget(int dinodeloc) {
     bool exist = false;
     int ind = dinodeloc % NHINO;
@@ -58,29 +71,34 @@ void iput(inode *pinode) {
                     bfree(pinode->dinode.di_addr[i]);
                 } else if (i == NADDR - 3)//一次间址
                 {
-                    unsigned int buff[BLOCKSIZ / sizeof(unsigned int)];
-                    fs.seekp(BLOCKSIZ * pinode->dinode.di_addr[i] + DATASTART, ios::beg);
-                    fs.read((char *) buff, sizeof(BlockCharge));
-                }
+                    undirect(pinode->dinode.di_addr[i], 0);
+                } else if (i == NADDR - 2)//两次间址
+                {
+                    undirect(pinode->dinode.di_addr[i], 1);
+                } else//三次间址
+                    undirect(pinode->dinode.di_addr[i], 2);
             }
-
         }
-    }
 
-    if (loc->i_ino == pinode->i_ino) {
-        hinode[ind] = loc->nx;
-        free(loc);
-        return;
     }
+}
 
-    while (loc->nx) {
-        inode *tmp = loc->nx;
-        if (tmp->i_ino == pinode->i_ino) {
-            loc->nx = tmp->nx;
-            free(tmp);
-            return;
-        }
-    }
+if (loc->i_ino == pinode->i_ino) {
+hinode[ind] = loc->
+nx;
+free(loc);
+return;
+}
+
+while (loc->nx) {
+inode *tmp = loc->nx;
+if (tmp->i_ino == pinode->i_ino) {
+loc->
+nx = tmp->nx;
+free(tmp);
+return;
+}
+}
 
 }
 
